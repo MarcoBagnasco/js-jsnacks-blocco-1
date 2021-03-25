50404adcaec0d2ecd8babb133b71698e1d2dda1a
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-25 19:20:23 +0100

    final review

--- js/script.js
+++ js/script.js
@@ -4,90 +4,97 @@ Il software deve chiedere per 5 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while
  */
-// var request = 5;
-// var sum = 0;
-// var items = '';
-// for(var i = 1; i <= request;i++){
-//     var number = parseInt(prompt('Inserisci numero ' + i + ' di ' + request));
-//     console.log(number);
-//     sum += number;
-//     items += '<li>' + number + '</li>';
-// }
-// document.getElementById('snack-1-list').innerHTML = items;
-// document.getElementById('snack-1-sum').innerHTML = sum;
-// console.log('Il totale dei numeri inseriti è:', sum);
+var request = 5;
+var sum = 0;
+var items = '';
+for(var i = 1; i <= request;i++){
+    var number = NaN;
+    while(isNaN(number)){
+        number = parseInt(prompt('Inserisci numero ' + i + ' di ' + request));
+    }
+    console.log(number);
+    sum += number;
+    items += '<li>' + number + '</li>';
+}
+document.getElementById('snack-1-list').innerHTML = items;
+document.getElementById('snack-1-sum').innerHTML = sum;
+console.log('Il totale dei numeri inseriti è:', sum);
 
 /* 
 Snack 2
 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
 */
-// var numero = parseInt(prompt('Inserisci un numero'));
+var numero = NaN;
+while(isNaN(numero)){
+    numero = parseInt(prompt('Inserisci un numero'));
+}
+document.getElementById('snack-2-number').innerHTML = numero;
 
-// if(numero % 2 === 0){
-//     console.log('Il numero è pari:', numero);
-//     document.getElementById('snack-2-number').innerHTML = 'Il numero è pari: ' + numero;
-// } else{
-//     document.getElementById('snack-2-number').innerHTML = 'Il numero è dispari. Stampo quello successivo: ' + (numero + 1);
+if(numero % 2 === 0){
+    console.log('Il numero è pari:', numero);
+    document.getElementById('snack-2-print').innerHTML = 'Il numero è pari: ' + numero;
+} else{
+    document.getElementById('snack-2-print').innerHTML = 'Il numero è dispari. Stampo quello successivo: ' + (numero + 1);
 
-//     console.log('Il numero è dispari. Stampo quello successivo:', numero + 1 );
-// }
+    console.log('Il numero è dispari. Stampo quello successivo:', numero + 1 );
+}
 
 /* 
 Snack 3
 Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 */
-// var firstNames = ['marco', 'paolo', 'giovanni', 'andrea', 'manuel'];
-// var items = '';
-// for(var i = 0; i < firstNames.length; i++){
-//     items += '<li>' + firstNames[i] + '</li>';
-// }
-// document.getElementById('snack-3-firstname-list').innerHTML = items;
-// console.log(firstNames);
+var firstNames = ['marco', 'paolo', 'giovanni', 'andrea', 'manuel'];
+var items = '';
+for(var i = 0; i < firstNames.length; i++){
+    items += '<li>' + firstNames[i] + '</li>';
+}
+document.getElementById('snack-3-firstname-list').innerHTML = items;
+console.log(firstNames);
 
-// var lastNames = ['rossi', 'verdi', 'bianchi', 'bagnasco', 'duzioni'];
-// items = '';
-// for(var i = 0; i < lastNames.length; i++){
-//     items += '<li>' + lastNames[i] + '</li>';
-// }
-// document.getElementById('snack-3-lastname-list').innerHTML = items;
-// console.log(lastNames);
+var lastNames = ['rossi', 'verdi', 'bianchi', 'bagnasco', 'duzioni'];
+items = '';
+for(var i = 0; i < lastNames.length; i++){
+    items += '<li>' + lastNames[i] + '</li>';
+}
+document.getElementById('snack-3-lastname-list').innerHTML = items;
+console.log(lastNames);
 
-// var guest = [];
-// var size = 3;
-// items = '';
-// for(var i = 0; i < size; i++){
-//     var fullName = firstNames[Math.floor(Math.random() * firstNames.length)] + ' ' + lastNames[Math.floor(Math.random() * lastNames.length)];
-//     if(!guest.includes(fullName)){
-//         guest.push(fullName);
-//         items += '<li>' + fullName + '</li>';
-//     } else{
-//         i--;
-//     }
-// }
-// document.getElementById('snack-3-guest-list').innerHTML = items;
-// console.log(guest);
+var guest = [];
+var size = 3;
+items = '';
+for(var i = 0; i < size; i++){
+    var fullName = firstNames[Math.floor(Math.random() * firstNames.length)] + ' ' + lastNames[Math.floor(Math.random() * lastNames.length)];
+    if(!guest.includes(fullName)){
+        guest.push(fullName);
+        items += '<li>' + fullName + '</li>';
+    } else{
+        i--;
+    }
+}
+document.getElementById('snack-3-guest-list').innerHTML = items;
+console.log(guest);
 
 /* 
 Snack 4
 Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 */
-// var numbers = [5, 6, 89, 36, 7, 8, 2, 45, 222, 456];
-// var items = '';
-// for(var i = 0; i < numbers.length; i++){
-//     items += '<li>' + numbers[i] + '</li>';
-// }
-// document.getElementById('snack-4-list').innerHTML = items;
-// console.log(numbers);
+var numbers = [5, 6, 89, 36, 7, 8, 2, 45, 222, 456];
+var items = '';
+for(var i = 0; i < numbers.length; i++){
+    items += '<li>' + numbers[i] + '</li>';
+}
+document.getElementById('snack-4-list').innerHTML = items;
+console.log(numbers);
 
-// var sum = 0;
+var sum = 0;
 
-// for(var i = 0; i < numbers.length; i++){
-//     if(i % 2 != 0){
-//         sum += numbers[i]
-//     }
-// }
-// document.getElementById('snack-4-sum').innerHTML = sum;
-// console.log(sum);
+for(var i = 0; i < numbers.length; i++){
+    if(i % 2 != 0){
+        sum += numbers[i]
+    }
+}
+document.getElementById('snack-4-sum').innerHTML = sum;
+console.log(sum);
 
 /* 
 Snack 5 - Bonus
