8bc6be4ad51b9ef3fb946221c52dd052a1863a2a
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-25 17:32:46 +0100

    add snack_3

--- js/script.js
+++ js/script.js
@@ -17,10 +17,30 @@ Esegui questo programma in due versioni, con il for e con il while
 Snack 2
 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
 */
-var numero = parseInt(prompt('Inserisci un numero'));
+// var numero = parseInt(prompt('Inserisci un numero'));
 
-if(numero % 2 === 0){
-    console.log('Il numero è pari:', numero);
-} else{
-    console.log('Il numero è dispari. Stampo quello successivo:', numero + 1 );
+// if(numero % 2 === 0){
+//     console.log('Il numero è pari:', numero);
+// } else{
+//     console.log('Il numero è dispari. Stampo quello successivo:', numero + 1 );
+// }
+
+/* 
+Snack 3
+Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+*/
+var firstNames = ['marco', 'paolo', 'giovanni', 'andrea', 'manuel'];
+console.log(firstNames);
+var lastNames = ['rossi', 'verdi', 'bianchi', 'bagnasco', 'duzioni'];
+console.log(lastNames);
+
+var guest = [];
+var size = 3;
+
+for(var i = 0; i < size; i++){
+    var fullName = firstNames[Math.floor(Math.random() * firstNames.length)] + ' ' + lastNames[Math.floor(Math.random() * lastNames.length)];
+    if(!guest.includes(fullName)){
+        guest.push(fullName);
+    }
 }
+console.log(guest);
