f10dbba759b3bcedbc3fbd6ffbca636cfeb9d2a7
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-25 18:37:46 +0100

    add style snack_2

--- js/script.js
+++ js/script.js
@@ -4,30 +4,33 @@ Il software deve chiedere per 5 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while
  */
-var request = 5;
-var sum = 0;
-var items = '';
-for(var i = 1; i <= request;i++){
-    var number = parseInt(prompt('Inserisci numero ' + i + ' di ' + request));
-    console.log(number);
-    sum += number;
-    items += '<li>' + number + '</li>';
-}
-document.getElementById('snack-1-list').innerHTML = items;
-document.getElementById('snack-1-sum').innerHTML = sum;
-console.log('Il totale dei numeri inseriti è:', sum);
+// var request = 5;
+// var sum = 0;
+// var items = '';
+// for(var i = 1; i <= request;i++){
+//     var number = parseInt(prompt('Inserisci numero ' + i + ' di ' + request));
+//     console.log(number);
+//     sum += number;
+//     items += '<li>' + number + '</li>';
+// }
+// document.getElementById('snack-1-list').innerHTML = items;
+// document.getElementById('snack-1-sum').innerHTML = sum;
+// console.log('Il totale dei numeri inseriti è:', sum);
 
 /* 
 Snack 2
 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
 */
-// var numero = parseInt(prompt('Inserisci un numero'));
+var numero = parseInt(prompt('Inserisci un numero'));
 
-// if(numero % 2 === 0){
-//     console.log('Il numero è pari:', numero);
-// } else{
-//     console.log('Il numero è dispari. Stampo quello successivo:', numero + 1 );
-// }
+if(numero % 2 === 0){
+    console.log('Il numero è pari:', numero);
+    document.getElementById('snack-2-number').innerHTML = 'Il numero è pari: ' + numero;
+} else{
+    document.getElementById('snack-2-number').innerHTML = 'Il numero è dispari. Stampo quello successivo: ' + (numero + 1);
+
+    console.log('Il numero è dispari. Stampo quello successivo:', numero + 1 );
+}
 
 /* 
 Snack 3
